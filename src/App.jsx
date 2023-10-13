6e4d66b9dfc1c9699d0943d784977755d23679bc
DevJaimeSantiago <jaimestgo475@gmail.com>
2023-10-13 13:47:40 -0500

    Guide Component completado

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 // Components
 import Hero from "./Components/Hero";
 import Companies from "./Components/Companies";
+import Guide from "./Components/Guide";
 
 const App = () => {
 	return (
 		<>
-		<Hero />
-		<Companies />
+			<Hero />
+			<Companies />
+			<Guide />
 		</>
 	);
 };
