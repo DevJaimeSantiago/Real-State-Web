12172f2fd71be6bfd99c6e1d4ac0fbf641c56d6e
DevJaimeSantiago <jaimestgo475@gmail.com>
2023-10-13 12:21:28 -0500

    Companies Component Completado

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 // Components
 import Hero from "./Components/Hero";
+import Companies from "./Components/Companies";
 
 const App = () => {
 	return (
 		<>
 		<Hero />
+		<Companies />
 		</>
 	);
 };
